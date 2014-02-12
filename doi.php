aec9bcfea47030ab4f7bcf28007aca230a53ad23
Harry Mustoe-Playfair <manticorp1234@gmail.com>
2014-02-12 18:45:51 +0000

    Test

--- doi.php
+++ doi.php
@@ -5,4 +5,10 @@ $doi = (isset($_REQUEST["doi"]))? $_REQUEST["doi"] : "10.1086/377226";
 
 //next example will recieve all messages for specific conversation
 $service_url = 'http://search.crossref.org/dois?q=' . urlencode($doi);
-echo file_get_contents($service_url);
+$service_url = 'http://search.crossref.org/?q=' . urlencode($doi);
+try{
+    $html = str_get_html( file_get_contents( $service_url ) );
+    echo $html->find('body',0)->plaintext;
+} catch(Exception $e){
+    echo "So this is it...";
+}
