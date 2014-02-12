b2724bdf94e253eba89ccd6dabe6381dc9f4ae11
Harry Mustoe-Playfair <manticorp1234@gmail.com>
2014-02-12 18:21:21 +0000

    Test

---
+++ test.php
@@ -0,0 +1,70 @@
+<html><head>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="jquery.zclip.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+<style>
+body{
+    font-size: 0.9em;
+}
+
+h1,h2,h3,h4,h5,h6, p, input, .btn {
+    font-family: 'Droid Sans', sans-serif;
+}
+p {
+
+}
+.btn {
+    cursor: pointer;
+    display: inline-block;
+    padding: 4px 6px;
+    border: 1px solid #ddd; 
+    border-radius: 4px; 
+    background: #eee;
+    text-decoration: none;
+    color: #333;
+    font-size: 0.8em;
+    margin: 4px;
+}
+
+.bibtex {
+    height: 1.2em;
+    margin-top: 4px;
+    margin-bottom: -4px;
+    display: inline-block;
+    background-image: url('img/BibTeX_logo.svg');
+    background-position: top left;
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 3.5em;
+}
+
+.latex {
+    height: 1.2em;
+    margin-top: 3px;
+    margin-bottom: -3px;
+    display: inline-block;
+    background-image: url('img/LaTeX_logo.svg');
+    background-position: top left;
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 3em;
+}
+
+input {
+    display: inline-block;
+    padding: 4px 6px;
+    border: 1px solid #ddd; 
+    border-radius: 4px; 
+    background: #eee;
+    color: #333;
+    margin: 4px;
+}
+
+.btn:hover {
+    box-shadow: 1px 1px 1px 1px rgba(0,0,0,0.03) inset;
+}
+</style>
+</head><body>
+<h1>This is a test</h1>
+</body></html>
